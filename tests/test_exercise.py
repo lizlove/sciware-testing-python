8a278fa3c56292866ee9b8682aac995bb3c38b96
lizlove <elizabethlovero@gmail.com>
2021-04-29 15:54:17 -0400

    Test written.

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,16 +3,18 @@ from sciware_testing_python import sum_numbers, add_vectors
 
 def test_sum_numbers_123():
     # basic test to see if we get the expected answer for a simple case.
-    sum = sum_numbers([1,2,3])
-    assert sum == 6
+    sum1 = sum_numbers([1,2,3])
+    assert sum1 == 6
 
 def test_sum_numbers_yours():
     # write another test of the sum_numbers function
-    pass
+    sum2 = sum_numbers([2, 4, 6])
+    assert sum2 == 12
 
 def test_sum_numbers_empty():
     # what's the sum of an empty list?
-    pass
+    sum3 = sum_numbers([])
+    assert sum3 == 0
 
 #@pytest.mark.xfail(strict=True, raises=TypeError)
 def test_sum_strings():
